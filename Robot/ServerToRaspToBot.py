c0c1c576ada52889c97231ba883e215ec12e71d5
LeDirge <valentinandral@hotmail.fr>
2023-06-30 15:43:16 +0100

    The command is sent only one time

--- Robot/ServerToRaspToBot.py
+++ Robot/ServerToRaspToBot.py
@@ -5,18 +5,9 @@ import time
 if __name__ == '__main__':
 	ser = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
 	ser.reset_input_buffer()
-	is_executed = False
-	ser.write(sys.argv[1].encode())
-	start_time = time.time()
-	timeout = 5
-	
-	while True:
-		if ser.in_waiting > 5:
-			line = ser.readline().decode('utf-8').rstrip()
-			print(line)
-			break
-		if time.time() - start_time > timeout:
-			print("Wot")
-			break	
-		
-	
+	command = sys.argv[1]
+	nb = ser.write("Wake up command".encode()) #create a connexion with the bot, allowing the real command to be received
+	time.sleep(5) # we give some time to the bot
+	nb = ser.write(command.encode()) # send the real command
+	print(ser.readline().decode('utf-8'))
+	ser.close()
